c0022ee0406c7214460e04398e65658fd04910ce
kato-takeshi05 <k024g1046@g.neec.ac.jp>
2025-04-11 14:41:07 +0900

    分からない ; ;

--- PG3-1/main.cpp
+++ PG3-1/main.cpp
@@ -1,7 +1,35 @@
 #include<stdio.h>
 
-int main() {
-	
+template<typename T>
+T min(T a, T b) {
+	if(a>b){
+		return static_cast<T>(b);
+	} else {
+		return static_cast<T>(a);
+	}
+}
+template<>
+float min<float>(float a, float b) {
+	if (a > b) {
+		return static_cast<float>(b);
+	} else {
+		return static_cast<float>(a);
+	}
+}
 
+template<>
+double min<double>(double a, double b) {
+	if (a > b) {
+		return static_cast<double>(b);
+	} else {
+		return static_cast<double>(a);
+	}
+}
+
+
+int main() {
+	printf("%d\n", min<int>(114, 514));
+	printf("%f\n", min<float>(11.4, 51.4));	
+	//
 	return 0;
 }

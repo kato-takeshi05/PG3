c2a03380163572c5469a95f2bbd5e6ffa4bb4cb2
kato-takeshi05 <k024g1046@g.neec.ac.jp>
2025-04-11 16:15:58 +0900

    出来た

--- PG3-1/main.cpp
+++ PG3-1/main.cpp
@@ -1,23 +1,29 @@
 #include<stdio.h>
-
-int Recursive(int time,int fast) {
+/// <summary>
+/// ‚PŠÔ“­‚­“x‚É‹‹‚ªŒ»İ‚Ì‹‹‚Ì‚Q”{-50‰~‚É‚È‚éÄ‹A“IŠÖ”
+/// </summary>
+/// <param name="time">˜J“­ŠÔ</param>
+/// <param name="payment">‰Šú‹‹</param>
+/// <returns>ÅI“I‚È‹‹</returns>
+int Recursive(int time,int payment) {
 	if (time <= 1) {
-		return(100);
+		return(payment);
 	}
-	
-	int result(fast * 2 - 50);
-
-	return(result+(Recursive(time-1,result)));
+	return(Recursive(time-1,payment*2-50));
 }
+//Ä‹AŠÖ”‚Å‹‹‚ª1ŠÔ‚²‚Æ‚ÉŒ»İ‚Ì‹‹‚Ì“ñ”{‚Ì-50‰~‚ª‹‹‚É‚È‚éŠÖ”‚ğì¬‚µ‚½‚¢B
+
 
 int main() {
-	int workTime = 3;
-	int fast = 100;
+	//˜J“­ŠÔ
+	int workTime = 8;
+	//‰Šú‹‹
+	int fastPayment = 100;
 	int commonResult;
 	int recursiveResult;
 
 	commonResult = 1072*workTime;
-	recursiveResult = Recursive(workTime,fast);
+	recursiveResult = Recursive(workTime,fastPayment);
 	printf("%d\n",commonResult);
 	printf("%d\n",recursiveResult);
 	return 0;

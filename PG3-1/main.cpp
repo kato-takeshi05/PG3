2abe48155dc720f94f7dc7502892ef81ce9b045e
kato-takeshi05 <k024g1046@g.neec.ac.jp>
2025-04-11 15:03:52 +0900

    階乗テスト

--- PG3-1/main.cpp
+++ PG3-1/main.cpp
@@ -1,7 +1,17 @@
 #include<stdio.h>
+int Recursive(int n) {
+	if (n <= 1) {
+		return(1);
+	}
 
+	return(n * Recursive(n - 1));
+}
 int main() {
-	
+	int workTime = 50;
+	int n = 3;
+	int result;
 
+	result = Recursive(n);
+	printf("%d‚ÌŠKæ=%d", n, result);
 	return 0;
 }

2b38d149f270098f328a29f08c604c2367fc8dc4
kato-takeshi05 <k024g1046@g.neec.ac.jp>
2025-04-11 15:16:08 +0900

    aaa

--- PG3-1/main.cpp
+++ PG3-1/main.cpp
@@ -9,12 +9,9 @@ T min(T a, T b) {
 	}
 }
 template<>
-float min<float>(float a, float b) {
-	if (a > b) {
-		return static_cast<float>(b);
-	} else {
-		return static_cast<float>(a);
-	}
+char min<char>(char a, char b) {
+	printf("•¶š—ñ‚Í‘}“ü‚Å‚«‚Ü‚¹‚ñB");
+		return 0;
 }
 
 
@@ -22,6 +19,8 @@ float min<float>(float a, float b) {
 int main() {
 	printf("%d\n", min<int>(114, 514));
 	printf("%f\n", min<float>(11.4, 51.4));	
+	//printf("%lf\n", min<double>(11.4514, 19,1919));	
+	printf("%c\n", min<char>('a','b'));
 	//
 	return 0;
 }

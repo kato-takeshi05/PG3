8dd59a90506a9776dc0f0e920a79aa825f9bd5fb
kato-takeshi05 <k024g1046@g.neec.ac.jp>
2025-04-11 16:33:31 +0900

    まじまんじ

--- PG3-1/main.cpp
+++ PG3-1/main.cpp
@@ -1,13 +1,18 @@
 #include<stdio.h>
 /// <summary>
+/// 合計給料
+/// </summary>
+int result = 0;
+/// <summary>
 /// １時間働く度に時給が現在の時給の２倍-50円になる再帰的関数
 /// </summary>
 /// <param name="time">労働時間</param>
-/// <param name="payment">初期時給</param>
-/// <returns>最終的な時給</returns>
+/// <param name="payment">時給</param>
+/// <returns>最終的な時給の合計金額</returns>
 int Recursive(int time,int payment) {
+	result = result + payment;
 	if (time <= 1) {
-		return(payment);
+		return(result);
 	}
 	return(Recursive(time-1,payment*2-50));
 }
@@ -16,7 +21,7 @@ int Recursive(int time,int payment) {
 
 int main() {
 	//労働時間
-	int workTime = 8;
+	int workTime = 2;
 	//初期時給
 	int fastPayment = 100;
 	int commonResult;
@@ -24,7 +29,7 @@ int main() {
 
 	commonResult = 1072*workTime;
 	recursiveResult = Recursive(workTime,fastPayment);
-	printf("%d\n",commonResult);
-	printf("%d\n",recursiveResult);
+	printf("一般的な給料:%d円\n",commonResult);
+	printf("再帰的な給料:%d円\n",recursiveResult);
 	return 0;
 }

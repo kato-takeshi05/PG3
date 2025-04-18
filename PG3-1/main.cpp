de1318622d9fe7660a843a036298dbf7344e3c45
kato-takeshi05 <k024g1046@g.neec.ac.jp>
2025-04-18 14:27:44 +0900

    わかりまへん

--- PG3-1/main.cpp
+++ PG3-1/main.cpp
@@ -1,10 +1,69 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+typedef void (*PFunc)(int*);
+
+void DispResult(int* s) {
+    printf("\n\nパカッ\n\n");
+}
+void setTimeout(PFunc p, int second) {
+    Sleep(second * 1000);
+
+    p(&second);
+}
+
+
+int rollDice() {
+    return rand() % 6 + 1;
+}
+int even(int number) {
+    if (number % 2 == 0) {
+        printf("正解");
+    } else {
+        printf("不正解");
+    }
+    return 0;
+}
+
+int odd(int number) {
+    if (number % 2 != 0) {
+        printf("正解");
+    } else {
+        printf("不正解");
+    }
+    return 0;
+}
 
 int main(int argc,const char *argv[]) {
+    srand(time(NULL));
 
-	auto fx = [](int i) { return i + 1; };
+    int dice = rollDice();
+    int insert = 0;
 
-	printf("%d", fx(2));
+
+
+
+
+
+    printf("サイコロを振りました\n");
+    printf("出た目が半(奇数)か丁(偶数)か当ててください\n");
+    printf("[1]:奇数 [2]:偶数\n");
+
+    scanf_s("%d", &insert);
+
+    PFunc p;
+    p = DispResult;
+    setTimeout(p, 3);
+
+    printf("出た目:%d\n", dice);
+    if (insert == 2) {
+        even(dice);
+    } else if (insert == 1) {
+        odd(dice);
+    }
+
 
 	return 0;
 }

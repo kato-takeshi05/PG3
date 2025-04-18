b445d6fa003bad91d015aad6bc9d18ddc9b50cd1
kato-takeshi05 <k024g1046@g.neec.ac.jp>
2025-04-18 14:50:18 +0900

    完成

--- PG3-1/main.cpp
+++ PG3-1/main.cpp
@@ -3,21 +3,37 @@
 #include <time.h>
 #include <windows.h>
 
+//コールバック関数
 typedef void (*PFunc)(int*);
 
+///
 void DispResult(int* s) {
     printf("\n\nパカッ\n\n");
 }
+
+/// <summary>
+/// コールバック関数を用いて時間を止める
+/// </summary>
+/// <param name="p">コールバック関数</param>
+/// <param name="second">止める秒数</param>
 void setTimeout(PFunc p, int second) {
     Sleep(second * 1000);
 
     p(&second);
 }
 
-
+/// <summary>
+/// ６面サイコロを振る
+/// </summary>
+/// <returns>出た目</returns>
 int rollDice() {
     return rand() % 6 + 1;
 }
+/// <summary>
+/// 数字が偶数かどうか判断する
+/// </summary>
+/// <param name="number">判断する数字</param>
+/// <returns>答え</returns>
 int even(int number) {
     if (number % 2 == 0) {
         printf("正解");
@@ -26,7 +42,11 @@ int even(int number) {
     }
         return 0;
 }
-
+/// <summary>
+/// 数字が奇数かどうか判断する
+/// </summary>
+/// <param name="number">判断する数字</param>
+/// <returns>答え</returns>
 int odd(int number) {
     if (number % 2 != 0) {
         printf("正解");
@@ -36,14 +56,14 @@ int odd(int number) {
     return 0;
 }
 
+
 int main() {
-    srand(time(NULL));
+    srand((unsigned)time(NULL));
 
     int dice = rollDice();
     int insert = 0;
     
-   
-
+    
 
 
   
@@ -53,16 +73,24 @@ int main() {
 
     scanf_s("%d",&insert);
 
+    int (*checkNum)(int);
+    if (insert == 2) {
+        checkNum = even;
+    } else if (insert == 1) {
+        checkNum = odd;
+
+    } else {
+        return 1;
+    }
+
     PFunc p;
     p = DispResult;
     setTimeout(p, 5);
 
-    printf("出た目:%d\n", dice);
-    if (insert == 2) {
-        even(dice);
-    } else if (insert == 1) {
-        odd(dice);
-    }
+    printf("出た目:%d\n",dice);
+   
+   
+    checkNum(dice);
 
     return 0;
 }

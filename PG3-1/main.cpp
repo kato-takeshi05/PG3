870ff577ba096a7a119a28584488918ce8ee7c6d
kato-takeshi05 <k024g1046@g.neec.ac.jp>
2025-05-23 15:15:19 +0900

    SS

--- PG3-1/main.cpp
+++ PG3-1/main.cpp
@@ -1,30 +1,21 @@
 #include <stdio.h>
 
-template <typename T1, typename T2>
-class Comparator {
-public:
-    T1 a;
-    T2 b;
-
-    Comparator(T1 a, T2 b) : a(a), b(b) {}
 
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 4; i++) {
         if (i < 1)
             man_parts[i] = new Penis;
         else
             man_parts[i] = new Ball;
 
     }
-
+    
     
 
-    printf("（int + int）: %d\n", num1.Min());
-    printf("（int + float）: %.2f\n", num2.Min());
-    printf("（int + double）: %.2f\n", num3.Min());
-    printf("（float + float ）: %.2f\n", num4.Min());
-    printf("（float + double）: %.2f\n", num5.Min());
-    printf("（double + double）: %.2f\n", num6.Min());
+    for (int i = 0; i < 3; i++) {
+        delete man_parts[i];
+    }
+
 
     return 0;
 }

60d242fa3ef1dfd43ff0b2218ddf32d56ae6cad8
kato-takeshi05 <k024g1046@g.neec.ac.jp>
2025-05-23 15:23:59 +0900

    AA

--- PG3-1/main.cpp
+++ PG3-1/main.cpp
@@ -1,21 +1,26 @@
-#include <stdio.h>
+#include<stdio.h>
+#include "Man.h"
+#include "Penis.h"
+#include "Ball.h"
+
+int main() {
+
+    Man* man_parts[3];
 
 
-
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 3; i++) {
         if (i < 1)
             man_parts[i] = new Penis;
         else
             man_parts[i] = new Ball;
 
     }
-    
-    
+
+
 
     for (int i = 0; i < 3; i++) {
         delete man_parts[i];
     }
-
 
     return 0;
 }

91c8c66c7dc798964d75fd29d6b5b60fe4ead637
kato-takeshi05 <k024g1046@g.neec.ac.jp>
2025-08-01 13:58:47 +0900

    a

--- PG3-1/main.cpp
+++ PG3-1/main.cpp
@@ -1,7 +1,51 @@
-#include<stdio.h>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+std::mutex mtx;
+std::condition_variable cv;
+int step = 1;
+
+/// <summary>
+/// thread1を出力する関数
+/// </summary>
+void printThread1() {
+    std::unique_lock<std::mutex> lock(mtx);
+    std::cout << "thread1\n";
+    step = 2;
+    cv.notify_all(); 
+}
+
+/// <summary>
+/// thread2を出力する関数
+/// </summary>
+void printThread2() {
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [] { return step == 2; }); 
+    std::cout << "thread2\n";
+    step = 3;
+    cv.notify_all();
+}
+
+/// <summary>
+/// thread3を出力する関数
+/// </summary>
+void printThread3() {
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, [] { return step == 3; }); 
+    std::cout << "thread3\n";
+}
 
 int main() {
-	printf("ソロモンよ私は帰ってきた");
-	//ガトー
-	return 0;
+    //スレッドを生成してそれぞれの関数を実行
+    std::thread t1(printThread1);
+    std::thread t2(printThread2);
+    std::thread t3(printThread3);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
 }
